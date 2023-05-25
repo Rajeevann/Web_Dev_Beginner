cbc146284d2e7298252c18bfdcdc530bba234847
Rajeevan <rajee.srs@gmail.com>
2023-05-24 20:31:12 -0700

    Add API Key

--- Receipe Book/index.js
+++ Receipe Book/index.js
@@ -1,5 +1,18 @@
-c;
+const API_KEY = "d888a4adb36446ba9eebf48ac9b2f514 ";
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.recipes;
+}
 
 async function init() {
   const recipes = await getRecipes();
+  console.log(recipes);
 }
+
+init();
