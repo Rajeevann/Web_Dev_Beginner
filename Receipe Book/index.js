da8e7902a2909d92b4a983bd558e8fd5e790cd9d
Rajeevan <rajee.srs@gmail.com>
2023-05-24 21:04:53 -0700

    Final JS

--- Receipe Book/index.js
+++ Receipe Book/index.js
@@ -1,4 +1,34 @@
 const API_KEY = "d888a4adb36446ba9eebf48ac9b2f514 ";
+const recipeListEl = document.getElementById("recipe-list");
+
+function displayRecipes(recipes) {
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-item");
+    recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe image";
+
+    recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerText = recipe.title;
+
+    recipeIngredientsEl = document.createElement("p");
+    recipeIngredientsEl.innerHTML = `<strong>Ingredients:</strong>${recipe.extendedIngredients
+      .map((ingredients) => ingredients.original)
+      .join(",")}`;
+
+    recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.innerText = "View Recipe";
+
+    recipeItemEl.appendChild(recipeImageEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientsEl);
+    recipeItemEl.appendChild(recipeLinkEl);
+    recipeListEl.appendChild(recipeItemEl);
+  });
+}
 
 async function getRecipes() {
   const response = await fetch(
@@ -12,7 +42,7 @@ async function getRecipes() {
 
 async function init() {
   const recipes = await getRecipes();
-  console.log(recipes);
+  displayRecipes(recipes);
 }
 
 init();
